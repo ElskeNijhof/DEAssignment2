e711627054608f218234dddf5d3b43a2c34ee165
ChrisDePekka <Christian15800@gmail.com>
2021-11-21 14:16:13 +0100

    hoi

--- Assignment2_DV/app.py
+++ Assignment2_DV/app.py
@@ -1,27 +1,25 @@
 import os
-import pandas as pd
-import requests
-from flask import Flask, Response,jsonify
+from flask import Flask
+from google.cloud import storage
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
+@app.route('/visualization/')
+def hello_world():
 
-@app.route('/visualizationass2/', methods=['GET'])
-def Visualization(age, clss):
-    #Set up returning text
-    html = "<h3>Hello, This is how you performed over the past week</h3>"  
+  storage_client = storage.Client()
+  file_data = 'out_ass2_batch'
+  bucket_name = 'output_batch_ass2'
+  temp_file_name = 'batch_output'
+  bucket = storage_client.get_bucket(bucket_name)
+  blob = bucket.get_blob(file_data)
+  blob.download_to_filename(temp_file_name)
 
-    #Retrieve the score that indicates 'live' or 'die'
-    Prediction_api = 'http://predict_service:5000/predict/Age:{}/class:{}'.format(age, clss)
-    Request = requests.get(Prediction_api)
-    value = float(Request.text)
-    
-    if value >= 0.5:
-        result = "live"
-        return html.format(liveOrDie = result)
-    else:
-        result = "die"
-        return html.format(liveOrDie = result)
+  temp_str=''
+  with open (temp_file_name, "r") as myfile:
+     temp_str = myfile.read().replace('\n', '')
 
-app.run(host='0.0.0.0', port=5000)
+  return temp_str
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080))) 
