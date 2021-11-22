c714d7ba9d59383f9924ed09d5383d4025e4a4ff
ChrisDePekka <Christian15800@gmail.com>
2021-11-22 11:47:55 +0100

    hoi

--- Assignment2_DV/app.py
+++ Assignment2_DV/app.py
@@ -7,21 +7,9 @@ import pandas as pd
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-
-#import gcsfs # type: ignore
-
-
 #@app.route('/visualization/week:<week>', methods=['GET'])
 @app.route('/visualization/')
 def hello_world():
-
-
-  # fs = gcsfs.GCSFileSystem(project='prefab-clover-330908')
-  # fs.ls('output_batch_ass2')
-  # with fs.open('output_batch_ass2/out_ass2_batch', 'rb') as f:
-  #   df = pd.read_csv(f)
-  #   return fs.ls('output_batch_ass2')
-
 
   storage_client = storage.Client()
   file_data = 'out_ass2_batch'
@@ -31,13 +19,14 @@ def hello_world():
   blob = bucket.get_blob(file_data)
   blob.download_to_filename(temp_file_name)
 
-  #df = pd.read_csv('gs://output_batch_ass2/out_ass2_batch')
   temp_str=''
+  header = ''
   with open (temp_file_name, "r") as myfile:
-    temp_str = myfile.readline()
+    header = myfile.readline()
+    temp_str = myfile.read()  # moet nog gefixt worden
     
-  return temp_str 
-  #return df.head()
+  return header, temp_str 
+
 
 #if __name__ == "__main__":
 #    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000))) 
