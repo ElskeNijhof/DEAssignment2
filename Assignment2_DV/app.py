4e2d293bad7b899c2122d6a4f573407e7e33b560
ChrisDePekka <Christian15800@gmail.com>
2021-11-29 10:48:21 +0100

    hoi

--- Assignment2_DV/app.py
+++ Assignment2_DV/app.py
@@ -30,8 +30,11 @@ def hello_world():
   return render_template("stream_good.html", result = read_from_topic())
 
 def read_from_topic():
-      kafka_consumer = KafkaConsumer(bootstrap_servers='34.123.176.107:9092',  # use your VM's external IP Here!
-      auto_offset_reset='latest',
+      kafka_consumer = KafkaConsumer('numtest', bootstrap_servers='34.123.176.107:9092',  # use your VM's external IP Here!
+      auto_offset_reset='earliest',
+      enable_auto_commit=True,
+      auto_commit_interval_ms = 1000,
+      group_id = 'group 6',
       consumer_timeout_ms=1000000)          # latest reads only latest values
       kafka_consumer.subscribe(topics=["output_stream"])
       
